c89bd57f4759c7733a6d92e1280829c08c723cb1
Tusi <Tusi@DESKTOP-CNN4D2B>
2020-06-08 20:45:54 -0600

    Creacion del Guia de ejercios, falta uno

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,374 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button_punt = New System.Windows.Forms.Button()
+        Me.Button_menos = New System.Windows.Forms.Button()
+        Me.Button_mas = New System.Windows.Forms.Button()
+        Me.Button_multi = New System.Windows.Forms.Button()
+        Me.Button_igual = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button_div = New System.Windows.Forms.Button()
+        Me.Button_CambioSigno = New System.Windows.Forms.Button()
+        Me.Button_limp = New System.Windows.Forms.Button()
+        Me.TextBox_result = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button_punt
+        '
+        Me.Button_punt.BackColor = System.Drawing.Color.Transparent
+        Me.Button_punt.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_punt.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_punt.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_punt.Location = New System.Drawing.Point(152, 242)
+        Me.Button_punt.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_punt.Name = "Button_punt"
+        Me.Button_punt.Size = New System.Drawing.Size(71, 51)
+        Me.Button_punt.TabIndex = 33
+        Me.Button_punt.Text = "."
+        Me.Button_punt.UseVisualStyleBackColor = False
+        '
+        'Button_menos
+        '
+        Me.Button_menos.BackColor = System.Drawing.Color.Transparent
+        Me.Button_menos.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_menos.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_menos.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_menos.Location = New System.Drawing.Point(222, 142)
+        Me.Button_menos.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_menos.Name = "Button_menos"
+        Me.Button_menos.Size = New System.Drawing.Size(71, 51)
+        Me.Button_menos.TabIndex = 32
+        Me.Button_menos.Text = "-"
+        Me.Button_menos.UseVisualStyleBackColor = False
+        '
+        'Button_mas
+        '
+        Me.Button_mas.BackColor = System.Drawing.Color.Transparent
+        Me.Button_mas.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_mas.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_mas.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_mas.Location = New System.Drawing.Point(222, 192)
+        Me.Button_mas.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_mas.Name = "Button_mas"
+        Me.Button_mas.Size = New System.Drawing.Size(71, 51)
+        Me.Button_mas.TabIndex = 31
+        Me.Button_mas.Text = "+"
+        Me.Button_mas.UseVisualStyleBackColor = False
+        '
+        'Button_multi
+        '
+        Me.Button_multi.BackColor = System.Drawing.Color.Transparent
+        Me.Button_multi.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_multi.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_multi.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_multi.Location = New System.Drawing.Point(222, 92)
+        Me.Button_multi.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_multi.Name = "Button_multi"
+        Me.Button_multi.Size = New System.Drawing.Size(71, 51)
+        Me.Button_multi.TabIndex = 29
+        Me.Button_multi.Text = "x"
+        Me.Button_multi.UseVisualStyleBackColor = False
+        '
+        'Button_igual
+        '
+        Me.Button_igual.BackColor = System.Drawing.Color.Transparent
+        Me.Button_igual.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_igual.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_igual.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_igual.Location = New System.Drawing.Point(222, 242)
+        Me.Button_igual.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_igual.Name = "Button_igual"
+        Me.Button_igual.Size = New System.Drawing.Size(71, 51)
+        Me.Button_igual.TabIndex = 28
+        Me.Button_igual.Text = "="
+        Me.Button_igual.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.Color.Transparent
+        Me.Button0.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button0.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(82, 242)
+        Me.Button0.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(71, 51)
+        Me.Button0.TabIndex = 27
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Transparent
+        Me.Button9.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(152, 92)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(71, 51)
+        Me.Button9.TabIndex = 26
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Transparent
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(82, 92)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(71, 51)
+        Me.Button8.TabIndex = 25
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Transparent
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(12, 92)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(71, 51)
+        Me.Button7.TabIndex = 24
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Transparent
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(152, 142)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(71, 51)
+        Me.Button6.TabIndex = 23
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Transparent
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(82, 142)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(71, 51)
+        Me.Button5.TabIndex = 22
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(12, 142)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(71, 51)
+        Me.Button4.TabIndex = 21
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(152, 192)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(71, 51)
+        Me.Button3.TabIndex = 20
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(82, 192)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(71, 51)
+        Me.Button2.TabIndex = 19
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 192)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 51)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button_div
+        '
+        Me.Button_div.BackColor = System.Drawing.Color.Transparent
+        Me.Button_div.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_div.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_div.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_div.Location = New System.Drawing.Point(222, 42)
+        Me.Button_div.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_div.Name = "Button_div"
+        Me.Button_div.Size = New System.Drawing.Size(71, 51)
+        Me.Button_div.TabIndex = 36
+        Me.Button_div.Text = "÷"
+        Me.Button_div.UseVisualStyleBackColor = False
+        '
+        'Button_CambioSigno
+        '
+        Me.Button_CambioSigno.BackColor = System.Drawing.Color.Transparent
+        Me.Button_CambioSigno.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_CambioSigno.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_CambioSigno.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_CambioSigno.Location = New System.Drawing.Point(12, 242)
+        Me.Button_CambioSigno.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_CambioSigno.Name = "Button_CambioSigno"
+        Me.Button_CambioSigno.Size = New System.Drawing.Size(71, 51)
+        Me.Button_CambioSigno.TabIndex = 35
+        Me.Button_CambioSigno.Text = "±"
+        Me.Button_CambioSigno.UseVisualStyleBackColor = False
+        '
+        'Button_limp
+        '
+        Me.Button_limp.BackColor = System.Drawing.Color.Transparent
+        Me.Button_limp.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button_limp.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_limp.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_limp.Location = New System.Drawing.Point(12, 40)
+        Me.Button_limp.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button_limp.Name = "Button_limp"
+        Me.Button_limp.Size = New System.Drawing.Size(211, 53)
+        Me.Button_limp.TabIndex = 37
+        Me.Button_limp.Text = "Limpiar"
+        Me.Button_limp.UseVisualStyleBackColor = False
+        '
+        'TextBox_result
+        '
+        Me.TextBox_result.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_result.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox_result.Name = "TextBox_result"
+        Me.TextBox_result.Size = New System.Drawing.Size(281, 31)
+        Me.TextBox_result.TabIndex = 40
+        Me.TextBox_result.Text = "0"
+        Me.TextBox_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(299, 40)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(231, 253)
+        Me.TextBox1.TabIndex = 41
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(634, 486)
+        Me.Controls.Add(Me.Button_div)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button_CambioSigno)
+        Me.Controls.Add(Me.TextBox_result)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button_limp)
+        Me.Controls.Add(Me.Button_igual)
+        Me.Controls.Add(Me.Button_menos)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button_mas)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button_punt)
+        Me.Controls.Add(Me.Button_multi)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button3)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button_punt As Button
+    Friend WithEvents Button_menos As Button
+    Friend WithEvents Button_mas As Button
+    Friend WithEvents Button_multi As Button
+    Friend WithEvents Button_igual As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button_limp As Button
+    Friend WithEvents Button_div As Button
+    Friend WithEvents Button_CambioSigno As Button
+    Friend WithEvents TextBox_result As TextBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+    Dim Operacion As String
+    Dim ValorResultado As Nullable(Of Double) = Nothing
+    Dim Valor2 As Nullable(Of Double) = Nothing
+    Dim CambioSigno As Double
+    Dim BotonOperacion As Boolean
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "1"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "9"
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        EvaluaRestriccionParaIngresar()
+        TextBox_result.Text &= "0"
+    End Sub
+
+    Private Sub Button_igual_Click(sender As Object, e As EventArgs) Handles Button_igual.Click
+        RealizarOperacion()
+        Operacion = ""
+    End Sub
+
+    Private Sub Button_mas_Click(sender As Object, e As EventArgs) Handles Button_mas.Click
+        RealizarOperacion()
+        Operacion = "+"
+        TextBox1.Text = TextBox1.Text & " " & ValorResultado & " " & Operacion
+    End Sub
+
+    Private Sub Button_menos_Click(sender As Object, e As EventArgs) Handles Button_menos.Click
+        RealizarOperacion()
+        Operacion = "-"
+    End Sub
+
+    Private Sub Button_multi_Click(sender As Object, e As EventArgs) Handles Button_multi.Click
+        RealizarOperacion()
+        Operacion = "*"
+    End Sub
+
+    Private Sub Button_div_Click(sender As Object, e As EventArgs) Handles Button_div.Click
+        RealizarOperacion()
+        Operacion = "/"
+    End Sub
+
+    Private Sub Button_punt_Click(sender As Object, e As EventArgs) Handles Button_punt.Click
+        EvaluaRestriccionParaIngresar()
+        If InStr(TextBox_result.Text, ".", CompareMethod.Text) = 0 Then
+            TextBox_result.Text &= "."
+        End If
+    End Sub
+
+    Private Sub Button_CambioSigno_Click(sender As Object, e As EventArgs) Handles Button_CambioSigno.Click
+        CambioSigno = Val(TextBox_result.Text) * -1
+        TextBox_result.Text = CambioSigno
+    End Sub
+
+
+    Private Sub Button_limp_Click(sender As Object, e As EventArgs) Handles Button_limp.Click
+        TextBox_result.Text = "0"
+        Valor2 = Nothing
+        ValorResultado = Nothing
+
+    End Sub
+    Public Sub RealizarOperacion()
+        BotonOperacion = True
+        Valor2 = Val(TextBox_result.Text) 'Valor2 = lo que haya en la caja de texto
+        If ValorResultado IsNot Nothing Then 'Si ValorRsultado no esta vacio
+            Select Case Operacion 'Se elije un case dependiendo del operador selecionado
+                Case "+"
+                    ValorResultado = ValorResultado + Valor2
+                    TextBox1.Text = ValorResultado & " +"
+                Case "-"
+                    ValorResultado -= Valor2
+                Case "*"
+                    ValorResultado *= Valor2
+                Case "/"
+                    ValorResultado = ValorResultado / Valor2
+
+            End Select
+            TextBox_result.Text = ValorResultado 'La caja de texto mostrara el resultado
+        Else
+            ValorResultado = Valor2 'Si ValorResultado esta vacio, ValorResultado = Valor2
+        End If
+
+    End Sub
+
+    Public Sub EvaluaRestriccionParaIngresar()
+        If BotonOperacion = True Then 'Si se invoco "RealizarOperacion"
+            TextBox_result.Text = "" 'Se limpia la caja de texto
+            BotonOperacion = False 'Bandera=false 
+        ElseIf TextBox_result.Text = "0" Then 'Si en la caja de texto hay 0
+            TextBox_result.Text = "" 'Entonces no hay nada y al ingresar algo este desaparece
+        End If
+    End Sub
+End Class
